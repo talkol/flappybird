87709d3d4e0d9c79c1e7e433752444cb5ae5aec0
Chengpeng Hu <chengpenghu@fb.com>
2020-03-04 10:43:55 -0800

    Adding support for Go controls, so the VR mode works in Oculus Browser on Go

--- button.js
+++ button.js
@@ -18,6 +18,7 @@ AFRAME.registerComponent("button", {
     let right = document.createElement("a-entity");
     right.setAttribute("vive-controls", "hand:right; model:false;");
     right.setAttribute("oculus-touch-controls", "hand:right; model:false;");
+    right.setAttribute("oculus-go-controls", "hand:right; model:false;");
     right.setAttribute("gearvr-controls", "hand:right; model:false;");
     right.setAttribute("windows-motion-controls", "hand:right; model:false;");
     right.addEventListener("triggerdown", function (evt) {
@@ -32,6 +33,7 @@ AFRAME.registerComponent("button", {
     let left = document.createElement("a-entity");
     left.setAttribute("vive-controls", "hand:left; model:false;");
     left.setAttribute("oculus-touch-controls", "hand:left; model:false;");
+    left.setAttribute("oculus-go-controls", "hand:left; model:false;");
     left.setAttribute("gearvr-controls", "hand:left; model:false;");
     left.setAttribute("windows-motion-controls", "hand:left; model:false;");
     left.addEventListener("triggerdown", function (evt) {
